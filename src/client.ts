1f7a9daf94dccca0189cc438a11f13a4ba90031f
Nguyễn Tiến Đạt <louiszn269@gmail.com>
2024-02-05 10:57:36 +0700

    feat: customized cache and sweepers

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import { Client, Collection, GatewayIntentBits } from "discord.js";
+import { Client, Collection, GatewayIntentBits, Options, Partials } from "discord.js";
 
 import { loadCommands, loadComponents, loadEvents } from "./utils/loader.js";
 import ClientUtils from "./utils/others/ClientUtils.js";
@@ -20,23 +20,50 @@ const client = new Client({
 		GatewayIntentBits.GuildVoiceStates,
 		GatewayIntentBits.GuildModeration,
 	],
+	partials: [
+		Partials.Message,
+		Partials.Reaction,
+		Partials.User,
+		Partials.GuildMember,
+		Partials.GuildScheduledEvent,
+	],
 	allowedMentions: {
 		repliedUser: false,
 		parse: [],
 	},
+	makeCache: Options.cacheWithLimits({
+		GuildMemberManager: {
+			maxSize: 0,
+			keepOverLimit: ({ id, client }) => id === client.user.id,
+		},
+		UserManager: {
+			maxSize: 0,
+			keepOverLimit: ({ id, client }) => id === client.user.id,
+		},
+	}),
+	sweepers: {
+		messages: {
+			interval: 60,
+			filter: () => (message) => {
+				const { createdTimestamp, author } = message;
+				return Date.now() - createdTimestamp >= 30 * 60_000 || author.bot;
+			},
+		},
+	},
 });
 
-client.commands = new Collection();
-client.components = {
-	buttons: new Collection(),
-	selectMenus: new Collection(),
-	modals: new Collection(),
-};
+client.process = process;
+
 client.config = config;
-client.utils = new ClientUtils(client);
+
+client.commands = new Collection();
+client.components = new Collection();
+
 client.mysql = new MySql(client.config.mysql);
 client.redis = new Redis(client.config.redis);
 client.managers = new Managers(client.mysql, client.redis);
+
+client.utils = new ClientUtils(client);
 
 await client.mysql.init();
 await client.redis.init();
