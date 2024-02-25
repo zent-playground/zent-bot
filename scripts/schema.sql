9a28a0c8f48579b6a382bf5758b9408130c7b726
Louis Johnson <louiszn269@gmail.com>
2024-02-25 20:23:02 +0700

    chore(TempVoice): new formation

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,47 +1,48 @@
 CREATE TABLE guilds (
-    id VARCHAR(36) NOT NULL PRIMARY KEY,
-    prefix VARCHAR(5) NOT NULL DEFAULT 'z',
+    `id` VARCHAR(36) NOT NULL PRIMARY KEY,
+    `prefix` VARCHAR(5) NOT NULL DEFAULT 'z',
     language VARCHAR(2) NOT NULL DEFAULT 'en',
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE users (
-    id VARCHAR(36) PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `id` VARCHAR(36) PRIMARY KEY,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE temp_voices (
-    id VARCHAR(36) PRIMARY KEY,
-    guild_id VARCHAR(36) NOT NULL,
-    author_id VARCHAR(36) NOT NULL,
-    creator_channel_id VARCHAR(36) NOT NULL,
-    claimed_by VARCHAR(36) NULL,
-    active BOOLEAN NOT NULL DEFAULT true,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (guild_id) REFERENCES guilds(id) ON DELETE CASCADE
+    `id` VARCHAR(36) PRIMARY KEY,
+    `guild_id` VARCHAR(36) NOT NULL,
+    `author_id` VARCHAR(36) NOT NULL,
+    `creator_channel_id` VARCHAR(36) NOT NULL,
+    `claimed_by` VARCHAR(36) NULL,
+    `active` BOOLEAN NOT NULL DEFAULT true,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`guild_id`) REFERENCES guilds(`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE temp_voice_creators (
-    id VARCHAR(36) NOT NULL PRIMARY KEY,
-    guild_id VARCHAR(36) NOT NULL,
-    affix VARCHAR(255) NULL,
-    generic_name VARCHAR(255) NULL,
-    generic_limit INT NULL,
-    allow_custom_name BOOLEAN NOT NULL DEFAULT false,
-    FOREIGN KEY (guild_id) REFERENCES guilds(id) ON DELETE CASCADE
+    `id` VARCHAR(36) NOT NULL PRIMARY KEY,
+    `guild_id` VARCHAR(36) NOT NULL,
+    `affix` VARCHAR(255) NULL,
+    `generic_name` VARCHAR(255) NULL,
+    `generic_limit` INT NULL,
+    `allow_custom_name` BOOLEAN NOT NULL DEFAULT false,
+    FOREIGN KEY (`guild_id`) REFERENCES guilds(`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE temp_voice_configs (
-    id VARCHAR(36) NOT NULL,
-    guild_id VARCHAR(36) NULL,
-    is_global BOOLEAN NOT NULL DEFAULT false,
+    `id` VARCHAR(36) NOT NULL,
+    `guild_id` VARCHAR(36) NULL,
+    `is_global` BOOLEAN NOT NULL DEFAULT false,
     name VARCHAR(255) NULL,
-    nsfw BOOLEAN NOT NULL DEFAULT false,
-    blacklisted_ids JSON,
-    whitelisted_ids JSON,
-    joinable INT NOT NULL DEFAULT 0,
-    user_limit INT NULL,
-    bitrate INT NOT NULL DEFAULT 64,
-    FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (guild_id) REFERENCES guilds(id) ON DELETE CASCADE
+    `nsfw` BOOLEAN NOT NULL DEFAULT false,
+    `blacklisted_ids` JSON,
+    `whitelisted_ids` JSON,
+    `lock` BOOLEAN NOT NULL DEFAULT false,
+	`hide` BOOLEAN NOT NULL DEFAULT false,
+    `user_limit` INT NULL,
+    `bitrate` INT NOT NULL DEFAULT 64,
+    FOREIGN KEY (`id`) REFERENCES users(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`guild_id`) REFERENCES guilds(`id`) ON DELETE CASCADE
 );
